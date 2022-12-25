7cd16e2a9b9707ae23d00a39618cc783a2dc3b29
Sonyamaster1 <sonyamitrofanova3262@gmail.com>
2022-12-25 23:57:32 +0500

    fix catch err

--- middlewares/handleError.js
+++
@@ -1,9 +0,0 @@
-module.exports = ((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode).send({
-    message: statusCode === 500
-      ? 'На сервере произошла ошибка'
-      : message,
-  });
-  next();
-});
